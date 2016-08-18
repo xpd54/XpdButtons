88e66ac68a43bbebd882ce67708112302daba9c5
Ravi Prakash <raviprakash.xpd54@gmail.com>
2016-08-18 23:51:50 +0530

    Initial commit

---
+++ Example/XpdButtons/XViewController.h
@@ -0,0 +1,13 @@
+//
+//  XViewController.h
+//  XpdButtons
+//
+//  Created by Ravi Prakash on 08/18/2016.
+//  Copyright (c) 2016 Ravi Prakash. All rights reserved.
+//
+
+@import UIKit;
+
+@interface XViewController : UIViewController
+
+@end
---
+++ Example/XpdButtons/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  XpdButtons
+//
+//  Created by Ravi Prakash on 08/18/2016.
+//  Copyright (c) 2016 Ravi Prakash. All rights reserved.
+//
+
+@import UIKit;
+#import "XAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XAppDelegate class]));
+    }
+}
